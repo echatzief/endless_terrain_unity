6c2dc42158de1d270d5a53e00d811a7dc16f6a5b
echatzief <echatzief97@gmail.com>
2020-12-20 19:00:55 +0200

    Assets for the basic endless terrain

---
+++ Assets/Scripts/MoveCameraScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCameraScript : MonoBehaviour{
+    
+    public float speed=2f;
+    public SpawnManager spawnManager;
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        float Horizontal = Input.GetAxis("Vertical");
+
+       
+        Vector3 moveDirection = new Vector3(Horizontal,0.0f,0.0f);
+        transform.position += moveDirection*speed;
+    }
+
+    private void OnTriggerEnter(){
+        print("Spawn Trigger");
+        spawnManager.SpawnTriggerEntered();
+    }
+}
---
+++ Assets/Scripts/RoadSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class RoadSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<GameObject> roads;
+    private float offset=4000f;
+    void Start()
+    {
+        if(roads !=null && roads.Count > 0){
+            //roads = roads.OrderBy(r=>r.transform.position.z).ToList();
+            roads = roads.OrderBy(r=> r.transform.position.x).ToList();
+        }
+    }
+
+    public void MoveRoad(){
+        GameObject moveRoad = roads[0];
+        roads.Remove(moveRoad);
+        float newX = roads[roads.Count - 1].transform.position.x + offset;
+        moveRoad.transform.position = new Vector3(newX,0,-120f);
+        roads.Add(moveRoad);
+    }
+}
